19d16534d3e8ffa575a709cc46aebe7577fd94bf
potatoo14 <92479313+potatoo14@users.noreply.github.com>
2025-09-24 09:14:23 -0300

    PEP8.py: descomenta verificação de dependências

--- scripts/PEP8.sh
+++ scripts/PEP8.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
-# pacotes=("isort" "black" "flake8")
-# for pkg in "${pacotes[@]}"; do
-#     (pip freeze | grep -iq "$pkg") || {
-#         >&2 echo "$pkg nao esta instalado"
-#         exit 1
-#     }
-# done
+pacotes=("isort" "black" "flake8")
+for pkg in "${pacotes[@]}"; do
+    (pip freeze | grep -iq "$pkg") || {
+        >&2 echo "$pkg nao esta instalado"
+        exit 1
+    }
+done
 
 echo "Rodando isort..."
 isort "$PROJECT_ROOT"
